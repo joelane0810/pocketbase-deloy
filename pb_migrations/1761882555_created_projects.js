c3266fcf8db34938e3e44c02582cde09973b2b39
joelane0810 <daituong0810@gmail.com>
2025-11-01 12:28:47 +0700

    Update 1761882555_created_projects.js

--- pb_migrations/1761882555_created_projects.js
+++ pb_migrations/1761882555_created_projects.js
@@ -129,7 +129,7 @@ migrate((app) => {
     "viewRule": null
   });
 
-  return app.save(collection);
+  return Dao(db).saveCollection(collection);
 }, (app) => {
   const collection = app.findCollectionByNameOrId("pbc_484305853");
 
